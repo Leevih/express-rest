ba174103e2dc5671f112bc682e0d725a5c71ee7f
Leevih <leevi.honkonen@gmail.com>
2019-12-02 13:30:47 +0200

    fixed my ports for heroku

--- app.js
+++ app.js
@@ -26,4 +26,7 @@ mongoose.connect(process.env.DB_CONNECTION, {  useUnifiedTopology: true , useNew
 });
 
 
-app.listen(8080);
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
